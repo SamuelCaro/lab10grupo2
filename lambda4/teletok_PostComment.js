f98f7b4c24264211241229632a92f8608a683119
Royer <r.yangali@pucp.pe>
2020-07-13 19:52:44 -0500

    pregunta 4

--- lambda4/teletok_PostComment.js
+++ lambda4/teletok_PostComment.js
@@ -1,6 +1,55 @@
-const mysql = require('mysql2');
+'use strict'
 
-exports.teletok_PostComment = (event, context, callback) => {
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-}
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "inventariotest"
+});
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Conexión exitosa a base de datos");
+    }
+});
+
+app.listen(3000, function () {
+    console.log("servidor levantado exitosamente");
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+////LOGICAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.post("/ws/post/comment", function (request, response) {
+    var token;
+    var postId;
+    var message;
+    token = request.body.token;
+    postId = request.body.postId;
+    message = request.body.message;
+    console.log(nombre);
+    console.log(postId);
+    console.log(message);
+    var sql = "INSERT INTO `inventariotest`.`categoriaequipo` (`nombre`) VALUES (?);";
+    var parametros = [nombre];
+    conn.query(sql, parametros, function (err, resultado) {
+        if (err) {
+            console.log(err);
+        } else {
+            var jason = {
+                "idCategoriaEquipo": resultado.insertId,
+                "nombre": nombre,
+            }
+            response.json(jason);
+        }
+    });
+
+});
