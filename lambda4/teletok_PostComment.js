ace0981650839152ac08b1d681e8fb91b45d99b7
Royer <r.yangali@pucp.pe>
2020-07-13 20:33:56 -0500

    gaaaaaaaaaaaaaaaaaaaaaaa

--- lambda4/teletok_PostComment.js
+++ lambda4/teletok_PostComment.js
@@ -29,27 +29,46 @@ app.listen(3000, function () {
 ////LOGICAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.post("/ws/post/comment", function (request, response) {
-    var token;
-    var postId;
-    var message;
-    token = request.body.token;
-    postId = request.body.postId;
-    message = request.body.message;
+
+    var token = request.body.token;
+    var postId = request.body.postId;
+    var message = request.body.message;
     console.log(nombre);
     console.log(postId);
     console.log(message);
-    var sql = "INSERT INTO `inventariotest`.`categoriaequipo` (`nombre`) VALUES (?);";
-    var parametros = [nombre];
-    conn.query(sql, parametros, function (err, resultado) {
-        if (err) {
-            console.log(err);
-        } else {
-            var jason = {
-                "idCategoriaEquipo": resultado.insertId,
-                "nombre": nombre,
+    var comprobacion = "SELECT * FROM token WHERE code= ?";
+    var parametro1=[token];
+    conn.query(comprobacion,parametro1);
+
+    if (comprobacion != null) {
+        var idmen = "SELECT u.id FROM teletok_lambda.token t INNER JOIN teletok_lambda.user u ON (t.user_id=u.id) WHERE t.code= ?";
+        var parametro2=[token];
+        conn.query(idmen,parametro2);
+
+        var sql = "INSERT INTO `teletok_lambda`.`post_comment` (`id`, `message`, `user_id`, `post_id`) VALUES ('',?, ?, ?)";
+        var parametros = [message, idmen, postId];
+        conn.query(sql, parametros, function (err, resultado) {
+            if (err) {
+                console.log(err);
+            } else {
+                var jason = {
+                    statusCode: 200,
+                    datos: {
+                        "commentId": resultado.insertId,
+                        "status": "COMMENT_CREATED"
+                    }
+                }
+                response.json(jason);
             }
-            response.json(jason);
+        });
+
+    } else {
+        var jason = {
+            statusCode: 400,
+            datos: {"error": "TOKEN_INVALID"}
         }
-    });
+        response.json(jason);
+    }
+
 
 });
